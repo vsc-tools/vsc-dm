f2d563c157d620dc061f8b7d2db740187590362b
Matthew Ballance <matt.ballance@gmail.com>
2023-09-13 11:03:20 -0700

    Add package library info
    Signed-off-by: Matthew Ballance <matt.ballance@gmail.com>

--- python/vsc_dm/__init__.py
+++ python/vsc_dm/__init__.py
@@ -1,3 +1,24 @@
 
+import os
+
 #from libvsc import core
 #from libvsc.core import *
+
+def get_deps():
+    return ["debug_mgr"]
+
+def get_libs():
+    return ["vsc-dm"]
+
+def get_libdirs():
+    pkg_dir = os.path.dirname(os.path.abspath(__file__))
+    return [pkg_dir]
+
+def get_incdirs():
+    pkg_dir = os.path.dirname(os.path.abspath(__file__))
+    if os.path.isdir(os.path.join(pkg_dir, "include")):
+        return [os.path.join(pkg_dir, "include")]
+    else:
+        root_dir = os.path.abspath(os.path.join(pkg_dir, "../.."))
+        return [os.path.join(root_dir, "src", "include")]
+
