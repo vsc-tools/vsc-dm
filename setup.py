7d3c2772a6cef75cd50197065c6f75ec942ae9b0
Matthew Ballance <matt.ballance@gmail.com>
2024-12-23 20:37:47 +0000

    Try vsc-dm tweak on PYTHONPATH
    Signed-off-by: Matthew Ballance <matt.ballance@gmail.com>

--- setup.py
+++ setup.py
@@ -6,11 +6,6 @@ import sys
 import platform
 from setuptools import Extension, find_namespace_packages
 
-if "IVPM_PYTHONPATH" in os.environ.keys():
-    ps = ";" if platform.system() == "Windows" else ":"
-    for i,p in enumerate(os.environ["IVPM_PYTHONPATH"].split(ps)):
-        sys.path.insert(i, p)
-
 version="0.0.1"
 
 proj_dir = os.path.dirname(os.path.abspath(__file__))
