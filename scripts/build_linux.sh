10f707f2a9d3af021c9157e30ef730df1cc98fa6
Matthew Ballance <matt.ballance@gmail.com>
2025-02-10 05:03:32 +0000

    XX
    Signed-off-by: Matthew Ballance <matt.ballance@gmail.com>

--- scripts/build_linux.sh
+++ scripts/build_linux.sh
@@ -1,8 +1,8 @@
 #!/bin/sh -x
 
 echo "BUILD_NUM=${BUILD_NUM}" >> python/vsc_dm/__build_num__.py
-${IVPM_PYTHON} -m pip install ivpm cython
-${IVPM_PYTHON} -m ivpm update -a
+${IVPM_PYTHON} -m pip install ivpm cython setuptools --pre
+${IVPM_PYTHON} -m ivpm update -a --py-prerls-packages
 
 PYTHON=./packages/python/bin/python
 ${PYTHON} -m pip install twine auditwheel ninja wheel cython
