f31cf6a0fbc5a4c1b78ef3cff50b84ce63ce0f7d
Matthew Ballance <matt.ballance@gmail.com>
2025-02-12 00:57:00 +0000

    Fix platform
    Signed-off-by: Matthew Ballance <matt.ballance@gmail.com>

--- scripts/build_linux.sh
+++ scripts/build_linux.sh
@@ -9,6 +9,6 @@ ${PYTHON} -m pip install twine auditwheel ninja wheel cython
 ${PYTHON} setup.py bdist_wheel
 
 for whl in dist/*.whl; do
-    ${PYTHON} -m auditwheel repair $whl
+    ${PYTHON} -m auditwheel repair --only-plat $whl
     rm $whl
 done
