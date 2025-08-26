41ad292b1fceb6c90996a58b9b1f740bb31fb625
Matthew Ballance <matt.ballance@gmail.com>
2025-08-26 02:30:22 +0000

    diagnosing httpx issue
    Signed-off-by: Matthew Ballance <matt.ballance@gmail.com>

--- scripts/build_linux.sh
+++ scripts/build_linux.sh
@@ -3,8 +3,13 @@
 yum install -y ninja-build
 
 echo "BUILD_NUM=${BUILD_NUM}" >> python/vsc_dm/__build_num__.py
-${IVPM_PYTHON} -m pip install ivpm cython setuptools --pre
+${IVPM_PYTHON} -m pip install ivpm cython setuptools
 ${IVPM_PYTHON} -m ivpm update -a --py-prerls-packages
+
+echo "httpx version"
+${IVPM_PYTHON} -m pip show httpx
+
+cat packages/python/lib64/python*/site-packages/httpx/_api.py
 
 PYTHON=./packages/python/bin/python
 ${PYTHON} -m pip install twine auditwheel ninja wheel cython
